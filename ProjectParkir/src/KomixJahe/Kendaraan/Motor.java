1f2469798cb006c94485bcc971acd16d1cfd731a
FirmanCitraNurrahman <firmancitranurrahman@gmail.com>
2018-12-05 10:12:56 +0700

    Class Motor ini merupakan subClass dari class Kendaraan

---
+++ ProjectParkir/src/KomixJahe/Kendaraan/Motor.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KomixJahe.Kendaraan;
+
+/**
+ *
+ * @author ASUS 10.1
+ */
+public class Motor extends Kendaraan{
+    private String namaKendaraan;
+    
+    public Motor(String noPolisi,String namaKendaraan) {
+        super(noPolisi);
+        this.namaKendaraan=namaKendaraan;
+    }
+
+    public String getNamaKendaraan() {
+        return namaKendaraan;
+    }
+
+    public void setNamaKendaraan(String namaKendaraan) {
+        this.namaKendaraan = namaKendaraan;
+    }
+
+    public String getNoPolisi() {
+        return noPolisi;
+    }
+
+    public void setNoPolisi(String noPolisi) {
+        this.noPolisi = noPolisi;
+    }
+    
+}
