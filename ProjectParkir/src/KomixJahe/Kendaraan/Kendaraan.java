6ad46b880f53479c5fae42b2b3d2bf6cb349858e
FirmanCitraNurrahman <firmancitranurrahman@gmail.com>
2018-12-05 10:10:49 +0700

    class ini merupakan parent class

---
+++ ProjectParkir/src/KomixJahe/Kendaraan/Kendaraan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KomixJahe.Kendaraan;
+
+/**
+ *
+ * @author ASUS 10.1
+ */
+public class Kendaraan {
+    protected String noPolisi;
+    private String namaKendaraan;
+    public Kendaraan(String noPolisi) {
+        this.noPolisi = noPolisi;
+    }
+
+    public String getNoPolisi() {
+        return noPolisi;
+    }
+
+    public void setNoPolisi(String noPolisi) {
+        this.noPolisi = noPolisi;
+    }
+    
+    
+}
