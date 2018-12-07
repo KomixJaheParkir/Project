72c71cf9f4a6037d668d77355661c5f89f63bef7
FirmanCitraNurrahman <firmancitranurrahman@gmail.com>
2018-12-07 21:59:36 +0700

    Class Operator merupakan bagian dari sub class User

---
+++ ProjectParkir/src/KomixJahe/User/Operator.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KomixJahe.User;
+
+/**
+ *
+ * @author ASUS 10.1
+ */
+public class Operator extends User {
+    private String nama;
+    private String jenisKelamin;
+    
+    public Operator(String id, String password) {
+        super(id, password);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+    
+}
